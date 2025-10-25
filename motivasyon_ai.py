e72767fa7d1664f397bdb892a8da7e51158b2d5a
ZeynebVarol <varolzeyneb12@gmail.com>
2025-10-25 17:09:19 +0300

    Create motivasyon_ai.py

---
+++ motivasyon_ai.py
@@ -0,0 +1,12 @@
+import random 
+
+motivasyon_sozleri = [
+  "Her kod satırı seni hayaline bir adım daha yaklaştırır.",
+  "Bugün küçük bir şey öğrendin mi? o zaman gelişiyorsun!",
+  "Yapay zekayı öğrenmek sabır ister ama senin sabrın yeter!",
+  "Hiç kimse başta mükemmel değildir, ama sen başlayarak öndesin.",
+  "Başarı sürekli öğrenmekten doğar.Devam et."
+]
+
+print("Yapay Zeka Motivatörü v1 .0")
+print(random.choice(motivsayon_sozleri))
